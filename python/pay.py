e19ee2d6f644f1b6ca5417b3d7920a56f2d4d420
MJ2P <mj2p.sg@gmail.com>
2015-05-05 23:12:23 +0100

    reformatted file in line with PEP8 guidelines

--- python/pay.py
+++ python/pay.py
@@ -26,32 +26,32 @@ import sys
 import json
 
 if len(sys.argv) == 1:
-  print "usage:", sys.argv[0], 'timestemp1 timestemp2 ...'
+    print "usage:", sys.argv[0], 'timestamp1 timestamp2 ...'
 
 users = {}
 credits = {}
 
 for ts in sys.argv[1:]:
-  try:
-    for line in open('logs/%s.log'%ts).readlines():
-      line = line.strip().split()
-      if line[2] == 'new':
-        if not line[7] in users:
-          users[line[7]] = []
-        users[line[7]].append(line[4])
-    for line in open('logs/%s.credits'%ts).readlines():
-      line = line.strip().split()
-      if not line[3] in credits:
-        credits[line[3]] = 0.0
-      if line[1] == '[-]':
-        credits[line[3]] -= float(line[2])
-      else:
-        credits[line[3]] += float(line[2])
-  except:
-    print >> sys.stderr, "could not read", ts
-    sys.exit(1)
+    try:
+        for line in open('logs/%s.log' % ts).readlines():
+            line = line.strip().split()
+            if line[2] == 'new':
+                if not line[7] in users:
+                    users[line[7]] = []
+                users[line[7]].append(line[4])
+        for line in open('logs/%s.credits' % ts).readlines():
+            line = line.strip().split()
+            if not line[3] in credits:
+                credits[line[3]] = 0.0
+            if line[1] == '[-]':
+                credits[line[3]] -= float(line[2])
+            else:
+                credits[line[3]] += float(line[2])
+    except:
+        print >> sys.stderr, "could not read", ts
+        sys.exit(1)
 
 out = {}
 for addr in users:
-  out[addr] = float("%.8f" % max(sum([credits[k] for k in users[addr] if k in credits]), 0.01))
+    out[addr] = float("%.8f" % max(sum([credits[k] for k in users[addr] if k in credits]), 0.01))
 print json.dumps(out).replace(' ', '')
