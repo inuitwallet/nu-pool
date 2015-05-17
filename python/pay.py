7a76bc2209513e52734c6719b343851731d0dce6
MJ2P <mj2p.sg@gmail.com>
2015-05-17 21:38:37 +0100

    updated pay.py to cope with rollover log files.

--- python/pay.py
+++ python/pay.py
@@ -21,6 +21,7 @@ DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR
 OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE
 OR OTHER DEALINGS IN THE SOFTWARE.
 """
+import os
 
 import sys
 import json
@@ -32,26 +33,41 @@ users = {}
 credits = {}
 
 for ts in sys.argv[1:]:
-    try:
-        for line in open('logs/%s.log' % ts).readlines():
-            line = line.strip().split()
-            if line[2] == 'new':
-                if not line[7] in users:
-                    users[line[7]] = []
-                users[line[7]].append(line[4])
-        for line in open('logs/%s.credits' % ts).readlines():
-            line = line.strip().split()
-            if not line[3] in credits:
-                credits[line[3]] = 0.0
-            if line[1] == '[-]':
-                credits[line[3]] -= float(line[2])
-            else:
-                credits[line[3]] += float(line[2])
-    except:
-        print >> sys.stderr, "could not read", ts
-        sys.exit(1)
+    # gather the users from the log file(s)
+    for data_file in os.listdir('logs'):
+        if ts not in data_file:
+            continue
+        if 'log' not in data_file:
+            continue
+        try:
+            for line in open('logs/%s' % data_file).readlines():
+                line = line.strip().split()
+                if line[2] == 'new':
+                    if not line[7] in users:
+                        users[line[7]] = []
+                    users[line[7]].append(line[4])
+        except:
+            print >> sys.stderr, "could not read", data_file
+            
+    # get the credits for the users from the credits file(s)            
+    for data_file in os.listdir('logs'):
+        if ts not in data_file:
+            continue
+        if 'credits' not in data_file:
+            continue
+        try:
+            for line in open('logs/%s' % data_file).readlines():
+                line = line.strip().split()
+                if not line[3] in credits:
+                    credits[line[3]] = 0.0
+                if line[1] == '[-]':
+                    credits[line[3]] -= float(line[2])
+                else:
+                    credits[line[3]] += float(line[2])
+        except:
+            print >> sys.stderr, "could not read", data_file
 
 out = {}
 for addr in users:
     out[addr] = float("%.8f" % max(sum([credits[k] for k in users[addr] if k in credits]), 0.01))
-print json.dumps(out).replace(' ', '')
+print '\'' + json.dumps(out).replace(' ', '') + '\''
