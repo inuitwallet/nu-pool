23d005e405aaf5cbca1adf6d07726def293c769f
sammoth <sam.griffiths@wazoku.com>
2015-08-17 21:31:01 +0100

    updated run_clent.py to allow for optional parameters

--- run_client.py
+++ run_client.py
@@ -9,6 +9,10 @@ config = json.load(open('pool.conf.json'))
 clients = []
 for conf in config:
     client = nupool.Client(conf['server'])
+    if 'deviation' not in conf:
+        conf['deviation'] = 0.0025
+    if 'reset_timer' not in conf:
+        conf['reset_timer'] = 0
     if not client.set(conf['api_key'], conf['api_secret'], conf['address'], conf['exchange'], conf['unit'], conf['bid_interest'],
                       conf['ask_interest'], conf['bot'], conf['order_match'], conf['deviation'], conf['reset_timer']):
         print "Failed to start client for %s on %s using %s" % (conf['unit'], conf['exchange'], conf['api_key'])
